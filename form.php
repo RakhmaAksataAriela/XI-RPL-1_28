28c7c31a2193e9db0db1ce707c71a668aa831f25
Rakhma Aksata <30462182+RakhmaAksataAriela@users.noreply.github.com>
2017-10-17 22:04:20 +0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Instagram</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div id ="container"> 
+
+	<div class="Header">
+		Instagram
+	</div>
+
+	<div class="subheader">
+		Sign up to see photos and videos <br>
+		from your friends.
+	</div>
+
+	<div class="button">
+		<form action="#" method="GET" >
+		<input type="submit" value="Login with Facebook">
+		</form>
+	</div>
+
+	<div class="content">
+		<form action="hasil.php" method="GET">
+			<input type="email" name="email" placeholder="Mobile Number or Email"><br>
+			<input type="text"	name="fullname" placeholder="Fullname"><br>
+			<input type="text"	name="user" placeholder="Username"><br>
+			<input type="password"	name="pw" placeholder="Password"><br>
+			<input type="submit" value="Sign Up">
+		</form>
+
+		
+	</div>
+
+		<div class="Footer">
+		By Signing Up, you agree to our <br>
+		Terms & Privacy Policy
+		</div>
+	
+</div>
+	
+</body>
+</html>
